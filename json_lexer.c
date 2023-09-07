1b538bf2028924c6a1ee1948780a99cb9260bf8d
Johannes Thorén <Johannes@lgjt.xyz>
2023-09-07 19:00:07 +0200

    wip

--- json_lexer.c
+++ json_lexer.c
@@ -6,11 +6,13 @@
 
 DEFINE_VECTOR_TYPE(JLTokenStream, jl, JLToken)
 
-JLTokenStream jl_new_token_stream() {
+JLTokenStream jl_new_token_stream()
+{
   return jl_new();
 }
 
-JLToken jl_new_token(JLTokenName token_name, JLValue token_value) {
+JLToken jl_new_token(JLTokenName token_name, JLValue token_value)
+{
   JLToken token;
   token.token_name = token_name;
   token.token_value = token_value;
@@ -18,74 +20,62 @@ JLToken jl_new_token(JLTokenName token_name, JLValue token_value) {
   return token;
 }
 
-void __add_separator_token(JLTokenStream *token_stream, const char c) {
-  JLValue value;
-  value.character = c;
-  JLToken token = jl_new_token(JSON_LEXER_TOKEN_SEPARATOR, value);
-  jl_push(token_stream, token);
-}
-void __add_key_token(JLTokenStream *token_stream, const char *str) {
-  JLValue value;
-  value.string = str;
-  JLToken token = jl_new_token(JSON_LEXER_TOKEN_KEY, value);
-  jl_push(token_stream, token);
+void __add_separator_token(JLTokenStream *token_stream, const char c)
+{
+  JLValue v = {.character = c};
+  jl_push(token_stream, jl_new_token(JSON_LEXER_TOKEN_SEPARATOR, v));
 }
 
-int __is_separator(const char c) {
-  for (int i = 0; i < strlen(JSON_LEXER_SEPARATORS); i++) {
-    if (c == JSON_LEXER_SEPARATORS[i])
-      return 0;
-  }
-
-  return 1;
+void __add_key_token(JLTokenStream *token_stream, const char *str)
+{
 }
 
-void jl_tokenize(JLTokenStream *token_stream, const char *str) {
-  int mode = JSON_LEXER_MODE_NORMAL;
+int jl_tokenize(JLTokenStream *token_stream, const char *str)
+{
+  int mode = JSON_LEXER_MODE_FIND_OBJECT_START_BRACKET;
   int str_ptr = 0;
   String store = new_string();
+  while (str_ptr < strlen(str))
+  {
+    switch (mode)
+    {
+    case JSON_LEXER_MODE_FIND_OBJECT_START_BRACKET:
+      if (str[str_ptr] != '{')
+      {
+        printf("formatting error");
+        return -1;
+      }
 
-  while (str_ptr < strlen(str)) {
-    switch (mode) {
-    case JSON_LEXER_MODE_NORMAL:
-      if (__is_separator(str[str_ptr]) == 0)
-        __add_separator_token(token_stream, str[str_ptr]);
-      if (__is_separator(str[str_ptr]) == 0 &&
-          str[str_ptr] == JSON_LEXER_SEPARATOR_QM) {
-        mode = JSON_LEXER_MODE_GET_KEY_NAME;
-        str_ptr++;
-        continue;
+      __add_separator_token(token_stream, str[str_ptr]);
+      mode = JSON_LEXER_MODE_FIND_KEY;
+
+      break;
+    case JSON_LEXER_MODE_FIND_KEY:
+      if (str[str_ptr] == '\"')
+      {
+        char *begin = str + str_ptr + 1;
+        char *end = strchr(begin, '\"');
+        if (end == NULL)
+        {
+          printf("Could not find closing \" for key");
+          return 0;
+        }
+
+        //__add_key_token(token_stream, )
+      }
+      else
+      {
+        printf("formatting error");
+        return 0;
       }
       break;
+    case JSON_LEXER_MODE_VALUE:
 
-    case JSON_LEXER_MODE_GET_KEY_NAME:
-
-      if (__is_separator(str[str_ptr]) == 0 &&
-          str[str_ptr] == JSON_LEXER_SEPARATOR_QM) {
-        mode = JSON_LEXER_MODE_NORMAL;
-        __add_key_token(token_stream, store.ptr);
-        store = new_string();
-        __add_separator_token(token_stream, str[str_ptr]);
-        str_ptr++;
-        continue;
-      }
-
-      string_push_char(&store, str[str_ptr]);
+      break;
+    default:
       break;
     }
+
     str_ptr++;
-  }
-}
-
-void jl_print_token_stream(const JLTokenStream token_stream) {
-  for (int i = 0; i < token_stream.length; i++) {
-    if (token_stream.ptr[i].token_name == JSON_LEXER_TOKEN_KEY ||
-        token_stream.ptr[i].token_name == JSON_LEXER_TOKEN_STRING) {
-      printf("(%d '%s')", token_stream.ptr[i].token_name,
-             token_stream.ptr[i].token_value.string);
-
-    } else
-      printf("(%d '%c')", token_stream.ptr[i].token_name,
-             token_stream.ptr[i].token_value.character);
   }
 }
--- json_lexer.h
+++ json_lexer.h
@@ -16,39 +16,31 @@
 #define JSON_LEXER_SEPARATOR_C JSON_LEXER_SEPARATORS[5]
 #define JSON_LEXER_SEPARATOR_CO JSON_LEXER_SEPARATORS[6]
 
-#define JSON_LEXER_MODE_NORMAL 0
-#define JSON_LEXER_MODE_GET_KEY_NAME 1
+// the mode to find the first curly bracket of a json object
+#define JSON_LEXER_MODE_FIND_OBJECT_START_BRACKET 0
+#define JSON_LEXER_MODE_FIND_KEY 1
+#define JSON_LEXER_MODE_VALUE 2 
 
-#define JSON_LEXER_TOKEN_JNULL 0
+#define JSON_LEXER_TOKEN_NULL 0
 #define JSON_LEXER_TOKEN_SEPARATOR 1
-#define JSON_LEXER_TOKEN_IDENTIFIER 2
-#define JSON_LEXER_TOKEN_KEY 3
-#define JSON_LEXER_TOKEN_NUMBER 4
-#define JSON_LEXER_TOKEN_JFLOAT 5
-#define JSON_LEXER_TOKEN_STRING 6
-#define JSON_LEXER_TOKEN_BOOLEAN 7
-#define JSON_LEXER_TOKEN_OBJ 8
+#define JSON_LEXER_TOKEN_KEYS 2
 
-typedef enum JLTokenName {
+typedef enum JLTokenName
+{
   JNULL = 0,
   SEPARATOR = 1,
-  IDENTIFIER = 2,
-  KEY = 3,
-  NUMBER = 4,
-  JFLOAT = 5,
-  STRING = 6,
-  BOOLEAN = 7,
-  OBJ = 8,
+  KEY = 2
 
 } JLTokenName;
 
-
-typedef union JLValue {
+typedef union JLValue
+{
   char character;
   char *string;
 } JLValue;
 
-typedef struct JLToken {
+typedef struct JLToken
+{
   JLTokenName token_name;
   JLValue token_value;
 } JLToken;
@@ -58,8 +50,8 @@ DECLARE_VECTOR_TYPE(JLTokenStream, jl, JLToken)
 JLToken jl_new_token(JLTokenName token_name, JLValue token_value);
 JLTokenStream jl_new_token_stream();
 
-void jl_tokenize(JLTokenStream *token_stream, const char *str);
+int jl_tokenize(JLTokenStream *token_stream, const char *str);
 
-void jl_print_token_stream(const JLTokenStream token_stream);
+// void jl_print_token_stream(const JLTokenStream token_stream);
 
 #endif
