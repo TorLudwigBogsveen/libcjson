7502fb7a5c46f384f2cc3b07b6bbe59c772193d4
Johannes Thorén <Johannes@lgjt.xyz>
2023-09-17 11:11:02 +0200

    does this work

--- json_lexer.c
+++ json_lexer.c
@@ -32,48 +32,53 @@ void __add_key_token(JLTokenStream *token_stream, const char *str)
 {
 }
 
-//kod tagen från https://www.geeksforgeeks.org/strtok-strtok_r-functions-c-examples/
-// Nested-Tokenization in C
+// kod tagen från https://www.geeksforgeeks.org/strtok-strtok_r-functions-c-examples/
+//  Nested-Tokenization in C
 
 int jl_tokenize(JLTokenStream *token_stream, const char *json_str)
 {
-  printf("%s\n",json_str);
-
-  printf("hello world\n");
+  printf("%s\n", json_str);
 
   char *str = malloc(sizeof(json_str));
 
-  printf("hello world\n");
-
   strcpy(str, json_str);
-
-  printf("hello world\n");
 
   const char outer_delimiters[] = ",";
   const char inner_delimiters[] = ":";
+  const char obj_delimiters[] = "{}";
 
   char *token;
   char *outer_saveptr = NULL;
   char *inner_saveptr = NULL;
+  char *obj_saveptr = NULL;
 
-  token = strtok_s(str, outer_delimiters, &outer_saveptr);
+  token = strtok_s(str, obj_delimiters, &obj_saveptr);
 
   while (token != NULL)
   {
-    printf("Outer Token: %s\n", token);
+    printf("Obj Token: %s\n", token);
 
-    char *inner_token = strtok_s(
-        token, inner_delimiters, &inner_saveptr);
+    char *outer_token = strtok_s(token, outer_delimiters, &outer_saveptr);
 
-    while (inner_token != NULL)
+    while (outer_token != NULL)
     {
-      printf("Inner Token: %s\n", inner_token);
-      inner_token = strtok_s(NULL, inner_delimiters,
-                             &inner_saveptr);
+      printf("Outer Token: %s\n", outer_token);
+
+      char *inner_token = strtok_s(
+          outer_token, inner_delimiters, &inner_saveptr);
+
+      while (inner_token != NULL)
+      {
+        printf("Inner Token: %s\n", inner_token);
+        inner_token = strtok_s(NULL, inner_delimiters,
+                               &inner_saveptr);
+      }
+
+      outer_token = strtok_s(NULL, outer_delimiters,
+                             &outer_saveptr);
     }
 
-    token = strtok_s(NULL, outer_delimiters,
-                     &outer_saveptr);
+    token = strtok_s(NULL, obj_delimiters, &obj_saveptr);
   }
 
   return 0;
